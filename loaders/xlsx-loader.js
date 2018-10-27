00b389808387e93c7eb1ae00a5ae1c5bda1bdaac
tangled1950 <44346917+tangled1950@users.noreply.github.com>
2018-10-26 20:30:43 -0400

    Update xlsx-loader.js

--- loaders/xlsx-loader.js
+++ loaders/xlsx-loader.js
@@ -1,22 +1,43 @@
 const funcBody = `
 ;;function write_ws_xml_datavalidation(validations) {
-  var o = '<dataValidations>';
-  for(var i=0; i < validations.length; i++) {
-    var validation = validations[i];
-    if(validation.type=="list"){
-      o += '<dataValidation type="list" allowBlank="1" sqref="' + validation.sqref + '">';
-      o += '<formula1>&quot;' + validation.values + '&quot;</formula1>';
-      o += '</dataValidation>';
-    }
-    if(validation.type=="decimal"){
-      o += '<dataValidation type="decimal" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
-      o += '<formula1>' + validation.min + '</formula1>';
-      o += '<formula2>' + validation.max + '</formula2>';
-      o += '</dataValidation>';
-    }
-  }
-  o += '</dataValidations>';
-  return o;
+	var o = '<dataValidations>';
+	for(var i=0; i < validations.length; i++) {
+		var validation = validations[i];
+		if(validation.type=="list"){
+			o += '<dataValidation type="list" allowBlank="1" sqref="' + validation.sqref + '" showInputMessage="1" showErrorMessage="1" errorTitle="sdfsadf" errorMessage="aaaadf">';
+			o += '<formula1>&quot;' + validation.values + '&quot;</formula1>';
+			o += '</dataValidation>';
+		}
+		else if(validation.type=="decimal"){
+			o += '<dataValidation type="decimal" operator="between" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
+			o += '<formula1>' + validation.min + '</formula1>';
+			o += '<formula2>' + validation.max + '</formula2>';
+			o += '</dataValidation>';
+		}
+		else if(validation.type=="date"){
+			o += '<dataValidation type="date" operator="between" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
+			o += '<formula1>' + (new Date(validation.start)).getTime()/1000 + '</formula1>';
+			o += '<formula2>' + (new Date(validation.end)).getTime()/1000 + '</formula2>';
+			o += '</dataValidation>';
+		}
+		else if(validation.type == "alphabet"){
+			o += '<dataValidation type="custom" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
+			o += '<formula1>ISNUMBER(SUMPRODUCT(SEARCH(MID('+validation.sqref.split(':')[0]+',ROW(INDIRECT(&quot;1:&quot;&amp;LEN('+validation.sqref.split(':')[0]+'))),1),&quot;abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&quot;)))</formula1>';
+			o += '</dataValidation>';
+		}
+		else if(validation.type == "email"){
+			o += '<dataValidation type="custom" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
+			o += '<formula1>=ISNUMBER(MATCH(&quot;*@*.???&quot;,'+validation.sqref.split(':')[0]+',0))</formula1>';
+			o += '</dataValidation>';
+		}
+		else if(validation.type == "phone_number"){
+			o += '<dataValidation type="custom" allowBlank="1" showInputMessage="1" showErrorMessage="1" sqref="' + validation.sqref + '">';
+			o += '<formula1>=AND(ISNUMBER(SUMPRODUCT(SEARCH(MID('+validation.sqref.split(':')[0]+',ROW(INDIRECT(&quot;1:&quot;&amp;LEN('+validation.sqref.split(':')[0]+'))),1),&quot;0123456789&quot;))),LEN('+validation.sqref.split(':')[0]+')=9,'+validation.sqref.split(':')[0]+'&lt;&gt;&quot;123456789&quot;,'+validation.sqref.split(':')[0]+'&lt;&gt;&quot;987654321&quot;,'+validation.sqref.split(':')[0]+'&lt;&gt;&quot;000000000&quot;)</formula1>';
+			o += '</dataValidation>';
+		}
+	}
+	o += '</dataValidations>';
+	return o;
 };;
 `;
 module.exports = function(input) {
@@ -28,4 +49,5 @@ module.exports = function(input) {
   `;
   return funcBody + input.substr(0, nextLineIndex) + funcCallCode + input.substr(nextLineIndex + 1);
 }
+
 
